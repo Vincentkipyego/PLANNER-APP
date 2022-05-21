99c3802470746005231cba06372cf1ad488185b8
vincent <vincentkipyego@gmail.com>
2022-05-21 16:02:02 +0300

    second phase

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -15,6 +15,20 @@ export default function App() {
     <Task  text= {'Task 2'}/>
     
     </View>
+
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    style= {styles.addTextWrapper}>
+    <TextInput style= {styles.input} placeholder ={"Write your Text"}/>
+
+    <TouchableOpacity>
+    <View style={styles.addWrapper}>
+    <Text style={styles.addtext}>+</Text>
+    </View>
+    </TouchableOpacity>
+    </KeyboardAvoidingView>
+    
+
     </View>
   );
 }
@@ -36,4 +50,31 @@ const styles = StyleSheet.create({
   items:{ 
     marginBottom : 30,
   },
+  addTextWrapper:{
+    position: 'absolute',
+    bottom : 60,
+    width :'100%',
+    flexDirection : 'row',
+    justifyContent :'space-around',
+    alignItems : 'center',
+  },
+  input:{
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    width :250,
+    backgroundColor : "#FFF",
+    borderWidth: 1,
+    borderRadius : 60,
+    borderColor :'#C0C0C0',
+  },
+  addWrapper: {
+    width: 60,
+    height :60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent :'center',
+    alignItems:'center',
+    borderWidth :1,
+  },
+  addtext:{},
 });
